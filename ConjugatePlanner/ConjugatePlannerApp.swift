04726c795cf44d4e9f7dd9efd889f6007a464211
David Sugden <david-sugden1@hotmail.co.uk>
2024-04-05 20:20:53 +0100

    Initial commit

---
+++ ConjugatePlanner/ConjugatePlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConjugatePlannerApp.swift
+//  ConjugatePlanner
+//
+//  Created by David Sugden on 05/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ConjugatePlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConjugatePlanner/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ConjugatePlanner
+//
+//  Created by David Sugden on 05/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
